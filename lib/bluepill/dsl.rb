ea7967d516e97f63e4b387c06278c7827c414a6f
ivanvanderbyl <ivanvanderbyl@me.com>
2010-09-30 14:16:37 +1000

    Changed error handling of top level DSL

--- lib/bluepill/dsl.rb
+++ lib/bluepill/dsl.rb
@@ -11,10 +11,11 @@ module Bluepill
       app = Application.new(app_name.to_s, options, &block)
       Blockenspiel.invoke(block, DSL::ApplicationMethods.new(app))
       
-      $stdout.print " Done.\n"
+      $stdout.print " Complete.\n"
       
       app.load
     rescue DSLConfigError => e
+      $stderr.puts " Failed.\n"
       $stderr.puts "Config error: #{e.message}"
       exit(6)
     end
